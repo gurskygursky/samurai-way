370233c29ba1b686b263db2a195914d0ea20b26d
yegorgursky <yahorhurski@gmail.com>
2022-10-09 20:58:13 +0300

    edit description, add avatar image

--- src/components/profile/ProfileDescription.tsx
+++ src/components/profile/ProfileDescription.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import avatarImage from './../../assets/images/rocket-ship-png.png';
 
 export const ProfileDescription = () => {
     return (
         <div>
-            avatar + description
+            <img style={{width: '64px', height: '64px'}}
+                 src={avatarImage}
+                 alt={'avatar logo'}
+            />
+            <span>Nickname:</span>
         </div>
     );
 };
