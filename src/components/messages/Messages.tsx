6dc8af89857f9998ba2a3e548864bc59565ad7fb
yegorgursky <yahorhurski@gmail.com>
2022-10-30 22:15:26 +0300

    lessons 21, fixed messages component

--- src/components/messages/Messages.tsx
+++ src/components/messages/Messages.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 export const Messages = () => {
     return (
-        <div style={{backgroundColor: 'blueviolet'}} className={'content'}>
-            <ul style={{listStyle: 'none'}}>
-                <li>
-                    <a href={'/dimych'}>Dimych</a>
-                </li>
-                <li style={{listStyle: 'none'}}>
-                    <a href={'/igor'}>Igor</a>
-                </li>
-                <li style={{listStyle: 'none'}}>
-                    <a href={'/katya'}>Katya</a>
-                </li>
-                <li style={{listStyle: 'none'}}>
-                    <a href={'/valera'}>Valera</a>
-                </li>
-                <li style={{listStyle: 'none'}}>
-                    <a href={'/viktor'}>Viktor</a>
-                </li>
-            </ul>
+        <div style={{backgroundColor: 'blueviolet', display: 'grid', gridTemplateColumns: '2fr 10fr'}}
+             className={'content'}>
+            <div>
+                <ul style={{listStyle: 'none'}}>
+                    <li>
+                        <NavLink to={'/dimych'} style={{textDecoration: 'none', cursor: 'pointer', color: 'black'}}>Dimych</NavLink>
+                    </li>
+                    <li style={{listStyle: 'none'}}>
+                        <NavLink to={'/igor'} style={{textDecoration: 'none', cursor: 'pointer', color: 'black'}}>Igor</NavLink>
+                    </li>
+                    <li style={{listStyle: 'none'}}>
+                        <NavLink to={'/katya'} style={{textDecoration: 'none', cursor: 'pointer', color: 'black'}}>Katya</NavLink>
+                    </li>
+                    <li style={{listStyle: 'none'}}>
+                        <NavLink to={'/valera'} style={{textDecoration: 'none', cursor: 'pointer', color: 'black'}}>Valera</NavLink>
+                    </li>
+                    <li style={{listStyle: 'none'}}>
+                        <NavLink to={'/viktor'} style={{textDecoration: 'none', cursor: 'pointer', color: 'black'}}>Viktor</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul style={{listStyle: 'none'}}>
+                    <li>Hi!</li>
+                    <li>How are you?</li>
+                    <li>Hello, IT-INC!</li>
+                </ul>
+                <textarea placeholder={'Enter your message'}/>
+            </div>
         </div>
     )
 }
