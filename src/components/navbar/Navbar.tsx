ea027a4435dcc1ccb90bd365b73aad3bd36c7aa0
yegorgursky <yahorhurski@gmail.com>
2022-10-30 21:49:04 +0300

    lesson 20, NavLink

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 export const Navbar = () => {
     return (
         <div style={{backgroundColor: 'gold'}} className={'nav'}>
             <ul style={{listStyle: 'none'}}>
                 <li>
-                    <a href={'/profile'}>Profile</a>
+                    {/*<a href={'/profile'}>Profile</a>*/}
+                    <NavLink to={'/profile'} style={{textDecoration: 'none'}}>Profile</NavLink>
                 </li>
-                <li style={{listStyle: 'none'}}>
-                    <a href={'/news'}>News</a>
+                <li>
+                    {/*<a href={'/news'}>News</a>*/}
+                    <NavLink to={'/news'} style={{textDecoration: 'none'}}>News</NavLink>
                 </li>
-                <li style={{listStyle: 'none'}}>
-                    <a href={'/messages'}>Messages</a>
+                <li>
+                    {/*<a href={'/messages'}>Messages</a>*/}
+                    <NavLink to={'/messages'} style={{textDecoration: 'none'}}>Messages</NavLink>
+
                 </li>
-                <li style={{listStyle: 'none'}}>
-                    <a href={'/music'}>Music</a>
+                <li>
+                    {/*<a href={'/music'}>Music</a>*/}
+                    <NavLink to={'/music'} style={{textDecoration: 'none'}}>Music</NavLink>
+
                 </li>
-                <li style={{listStyle: 'none'}}>
-                    <a href={'/settings'}>Settings</a>
+                <li>
+                    {/*<a href={'/settings'}>Settings</a>*/}
+                    <NavLink to={'/settings'} style={{textDecoration: 'none'}}>Settings</NavLink>
                 </li>
             </ul>
         </div>
