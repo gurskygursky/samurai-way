b677236291ab64e10561e14bec0d1a82444b5ce7
yegorgursky <yahorhurski@gmail.com>
2022-12-12 17:58:24 +0300

    less 53, 54, class extends React.Component, life cycle methods, componentDidMount

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -4,15 +4,41 @@ import avatar_male_user from './../../assets/images/avatar_male_user.png'
 import {instance, UserResponseType, UsersResponseType} from './../../API/users-api';
 import axios from 'axios';
 
+export class Users extends React.Component<UsersContainerPropsType, any>{
 
-export const Users = (props: UsersContainerPropsType) => {
-
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            instance.get(`users`)
-                .then(res => props.setUsers(res.data.items));
-        }
+    componentDidMount() {
+        instance.get(`users`)
+            .then(res => this.props.setUsers(res.data.items));
     }
+
+    render() {
+        return (
+            <div style={{backgroundColor: 'blueviolet'}} className={'content'}>
+                {this.props.users.map((user) => {
+                    return (
+                        <div key={user.id}>
+                            <img style={{width: '64px', height: '64px'}} src={user.photos.small !== null ? user.photos.small : avatar_male_user} alt={'user_image_avatar'}/>
+                            <div style={{color: 'black'}}>{user.name}</div>
+                            <div>{user.status}</div>
+                            {user.followed
+                                ? <button onClick={() => this.props.unfollow(user.id)}>FOLLOW</button>
+                                : <button onClick={() => this.props.follow(user.id)}>UNFOLLOW</button>
+                            }
+                        </div>
+                    )
+                })}
+            </div>
+        );
+    }
+}
+// export const Users = (props: UsersContainerPropsType) => {
+
+    // let getUsers = () => {
+    //     if (props.users.length === 0) {
+    //         instance.get(`users`)
+    //             .then(res => props.setUsers(res.data.items));
+    //     }
+    // }
     // props.setUsers([
     //     {
     //         id: 1, photos: avatar_male_user, fullName: 'Dimych', isFollow: false,
@@ -46,22 +72,22 @@ export const Users = (props: UsersContainerPropsType) => {
     // );
     // }
 
-    return (
-        <div style={{backgroundColor: 'blueviolet'}} className={'content'}>
-            <button onClick={getUsers}>Get Users</button>
-            {props.users.map((user) => {
-                return (
-                    <div key={user.id}>
-                        <img style={{width: '64px', height: '64px'}} src={user.photos.small !== null ? user.photos.small : avatar_male_user} alt={'user_image_avatar'}/>
-                        <div style={{color: 'black'}}>{user.name}</div>
-                        <div>{user.status}</div>
-                        {user.followed
-                            ? <button onClick={() => props.unfollow(user.id)}>FOLLOW</button>
-                            : <button onClick={() => props.follow(user.id)}>UNFOLLOW</button>
-                        }
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+//     return (
+//         <div style={{backgroundColor: 'blueviolet'}} className={'content'}>
+//             <button onClick={getUsers}>Get Users</button>
+//             {props.users.map((user) => {
+//                 return (
+//                     <div key={user.id}>
+//                         <img style={{width: '64px', height: '64px'}} src={user.photos.small !== null ? user.photos.small : avatar_male_user} alt={'user_image_avatar'}/>
+//                         <div style={{color: 'black'}}>{user.name}</div>
+//                         <div>{user.status}</div>
+//                         {user.followed
+//                             ? <button onClick={() => props.unfollow(user.id)}>FOLLOW</button>
+//                             : <button onClick={() => props.follow(user.id)}>UNFOLLOW</button>
+//                         }
+//                     </div>
+//                 )
+//             })}
+//         </div>
+//     )
+// }
